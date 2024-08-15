e03e3c248852cccf410168a6875e6756b8a57a90
LittleChipsV <andika.rofif2021@gmail.com>
2024-08-15 17:54:23 +0700

    setup awal block theme

--- front-page.php
+++
@@ -1,7 +0,0 @@
-<?php get_header(); ?>
-
-<main id="main-content" role="main">
-
-</main>
-
-<?php get_footer(); ?>
--- functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once get_parent_theme_file_path( 'includes/classes/class-tema-breezelabs.php' );
-
-if ( class_exists('TemaBreezelabs') ) {
-   new TemaBreezelabs();
-}
